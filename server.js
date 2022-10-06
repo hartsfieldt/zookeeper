88fc2332a368939505b878f74976a83cc668e35e
Your Name <you@example.com>
2022-10-06 10:19:52 -0400

    finished creating the users feature to create a new animal and validate their creation.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 
 const express = require("express");
 const { animals } = require("./data/animals");
@@ -14,13 +16,13 @@ function filterByQuery(query, animalsArray) {
   if (query.personalityTraits) {
     // Save personalityTraits as a dedicated array.
     // If personalityTraits is a string, place it into a new array and save.
-    if (typeof query.personalityTraits === 'string') {
+    if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
     // Loop through each trait in the personalityTraits array:
-    personalityTraitsArray.forEach(trait => {
+    personalityTraitsArray.forEach((trait) => {
       // Check the trait against each animal in the filteredResults array.
       // Remember, it is initially a copy of the animalsArray,
       // but here we're updating it for each trait in the .forEach() loop.
@@ -29,34 +31,58 @@ function filterByQuery(query, animalsArray) {
       // so at the end we'll have an array of animals that have every one
       // of the traits when the .forEach() loop is finished.
       filteredResults = filteredResults.filter(
-        animal => animal.personalityTraits.indexOf(trait) !== -1
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
     });
   }
   if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
   }
   if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
   }
   if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
   }
   return filteredResults;
 }
 
 function findById(id, animalsArray) {
-  const results = animalsArray.filter(animal => animal.id === id)[0];
+  const results = animalsArray.filter((animal) => animal.id === id)[0];
   return results;
 }
 
 function createNewAnimal(body, animalsArray) {
-  console.log(body);
-
-
-  return body;
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
 }
 
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 
 // To add a route
 app.get("/api/animals", (req, res) => {
@@ -70,19 +96,25 @@ app.get("/api/animals", (req, res) => {
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
-  res.json(result);
-} else {
-  res.send(404);
-}
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.post("/api/animals", (req, res) => {
-   // set id based on what the next index of the array will be
+  // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
-  res.json(req.body);
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 // To listen to the port
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
